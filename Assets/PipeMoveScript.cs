d7d4bf813d477b56202537160f5aacb50fe55160
Iruka Pathirana <thuthinidhijpic.20@uom.lk>
2024-07-26 17:39:41 +0530

    Score Initiated

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -16,6 +16,10 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if(transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
